68cae857587f834a1a73ea5529436b4ace28ac09
tasnim <tasnim.mami@ensi-uma.tn>
2023-03-30 22:04:37 +0100

    firstcommit

---
+++ src/explswing/welcome.java
@@ -0,0 +1,18 @@
+package explswing;
+
+import javax.swing.JFrame;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		
+		// TODO Instancier une JFrame 
+		
+		JFrame mafenet = new JFrame() ;
+		
+		// TODO Afficher la JFrame
+		mafenet.setVisible(true);	
+
+	}
+
+}
